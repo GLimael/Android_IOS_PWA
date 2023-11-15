90a02c42f9a3455e0a04503cbaad7444d7b366b0
GLimael <gabriel23052006@gmail.com>
2023-11-14 21:55:25 -0300

    finalizando

--- Lista-de-listas/Lista-de-Listas/src/EditorLista/EditorLista.js
+++ Lista-de-listas/Lista-de-Listas/src/EditorLista/EditorLista.js
@@ -1,21 +1,45 @@
-import {useState} from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { useEffect, useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import metadata from '../storage.metadata.json';
+import { useIsFocused } from '@react-navigation/native';
 
 
-export default function HomeScreen() {
+export default function HomeScreen({ navigation, route }) {
+
+
+    const nome = route.params?.nomeLista;
+
+    
+    const [lista, setLista] = useState([]);
     const [nomeLista, setNomeLista] = useState("");
+    const focus = useIsFocused();
+    useEffect(() => {getLista()}, [focus]);
 
-    const salvarNomeLista = async () => {
-        const salvarNome = nomeLista || "";
-        await AsyncStorage.setItem(metadata.LISTA.NOMELISTA, salvarNome);
+    const getLista = async () => {
+        const listaTemporaria = await AsyncStorage.getItem(metadata.LISTA);
+        if (typeof JSON.parse(listaTemporaria) != "object" || JSON.parse(listaTemporaria) == null) {
+            setLista([])
+        }else {
+            setLista(JSON.parse(listaTemporaria));
+        }
+    }
+
+    console.log(lista, typeof lista)
+    const salvar = async () => {
+        let variavel = nomeLista === "" ? [...lista] : [...lista, nomeLista];
+        console.log("variavel: ", variavel, typeof variavel)
+        await AsyncStorage.setItem(metadata.LISTA, JSON.stringify(variavel));
+        nomeLista !== "" ? navigation.navigate('Home') : alert("Digite algo!");
     }
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Adicionar Lista</Text>
             <TextInput placeholder='Nome da lista' value={nomeLista} onChangeText={setNomeLista} />
+            <TouchableOpacity onPress={salvar}>
+                <Text>Salvar Lista</Text>
+            </TouchableOpacity>
         </View>
     );
 }
--- Lista-de-listas/Lista-de-Listas/src/HomeScreen/HomeScreen.js
+++ Lista-de-listas/Lista-de-Listas/src/HomeScreen/HomeScreen.js
@@ -1,16 +1,37 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useEffect, useState} from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import metadata from '../storage.metadata.json';
+import { useIsFocused } from '@react-navigation/native';
 
 export default function HomeScreen({ navigation }) {
-    const [lista, setLista] = useState("");
-    useEffect(() => {setLista()}, []);
+    const [lista, setLista] = useState([]);
+    const focus = useIsFocused();
+    useEffect(() => {buscar()}, [focus]);
+
+    const buscar = async () => {
+        const itens = await AsyncStorage.getItem(metadata.LISTA);
+        setLista(JSON.parse(itens));
+    }
+
+    
+    console.log("Home: ", lista, typeof lista)
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
-            <TouchableOpacity onPress={navigation.navigate('EditorLista')} >
+            <TouchableOpacity onPress={() => {navigation.navigate('EditorLista', {nomeLista: "bruno"})}} >
                 <Text>Adicionar Lista</Text>
             </TouchableOpacity>
+            {lista && 
+                lista.map((nome, index) => {
+                    return (
+                        <View key={index}>
+                            <Text>{nome}</Text>
+                        </View>
+                    )
+                })
+            }
             
         </View>
     );
